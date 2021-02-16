eafee58e62b273aa3b65e497a4e4efc89f9e0715
albailey <Al.Bailey@windriver.com>
2021-02-16 12:15:30 -0600

    Not require recreate of tox env when running tox
    The upgrade test was written to copy a file required for
    the test, but did not handle the file already being processed.
    This fix adjusts the setup for the unit test to handle this
    and allows the recreate to be removed, thus reducing designer
    tox testing time (40 seconds down to 4 when re-running a single test)
    
    This also aligns the version of hacking and pylint as the same as
    other starlingx repos. A new pylint was released which was
    breaking tox.
    
    The code changes are to fix whitespace (rather than suppressing):
     E305 expected 2 blank lines after class or function definition
     E117 over-indented
    
    Three new hacking warnings are being suppressed:
      W503 line break before binary operator
      W504  line break after binary operator
      W605 invalid escape sequence
    
    Un-suppress these codes which are not broken in the code:
     E123 closing bracket does not match indentation of opening bracket
     E501 line too long (tox sets max length instead to a large val)
     F823 local variable referenced before assignment
    
    yamllint 1.26.1 does not work with py2 or py3 in our env, so clamp it.
    
    Story: 2008137
    Task: 42200
    Signed-off-by: albailey <Al.Bailey@windriver.com>
    Change-Id: Ia623315c5f1a690d20f597242b7722601776f26d

--- nfv/nfv-tests/nfv_unit_tests/tests/test_database_upgrades.py
+++ nfv/nfv-tests/nfv_unit_tests/tests/test_database_upgrades.py
@@ -4,7 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 import os
+import shutil
 import subprocess
+import tempfile
 
 from nfv_vim import database
 from nfv_vim import tables
@@ -14,24 +16,34 @@ from . import testcase  # noqa: H304
 
 class TestNFVDatabaseUpgrade(testcase.NFVTestCase):
 
+    def setUp(self):
+        super(TestNFVDatabaseUpgrade, self).setUp()
+        root_dir = os.environ['VIRTUAL_ENV']
+        # create a directory to hold the DB, randomly named, under the tox env
+        self.db_dir = tempfile.mkdtemp(dir=root_dir)
+
+    def tearDown(self):
+        super(TestNFVDatabaseUpgrade, self).tearDown()
+        shutil.rmtree(self.db_dir)
+
     def test_nfv_vim_database_upgrade_from_19_12(self):
         """
         Test VIM database upgrades from stx 19_12
         """
         root_dir = os.environ['VIRTUAL_ENV']
-
+        # stage some old data
         devnull = open(os.devnull, 'w')
         try:
             vim_cmd = ("nfv-vim-manage db-load-data -d %s "
-                       "-f %s/nfv_vim_db_stx_19.12" % (root_dir, root_dir))
+                       "-f %s/nfv_vim_db_stx_19.12" % (self.db_dir, root_dir))
 
             subprocess.check_call([vim_cmd], shell=True, stderr=devnull)
-
         except subprocess.CalledProcessError:
             raise
 
+        # migrate the old data
         config = dict()
-        config['database_dir'] = root_dir
+        config['database_dir'] = self.db_dir
         database.database_initialize(config)
         database.database_migrate_data()
         tables.tables_initialize()
